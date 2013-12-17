409c469848a9105d7a7e8dd0769c32404146705e
Adam Midvidy <amidvidy@gmail.com>
2013-12-17 15:04:00 -0800

    docstring formatting

--- multimatch/asset.py
+++ multimatch/asset.py
@@ -1,7 +1,11 @@
 DELIM = '/'
 
 def make_symbol(*args):
-    """Returns a canonical symbol for an arbitrary set of parameters
-    e.g. make_symbol('BTC', 'USD') => 'BTC/USD'"""
+    """
+    Returns a canonical symbol for an arbitrary set of parameters
+    
+    example: make_symbol('BTC', 'USD') => 'BTC/USD'
+    
+    """
     
     return DELIM.join(sorted(map(str, args)))
