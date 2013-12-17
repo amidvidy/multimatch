d16a8c03001c5b610005661532dafb711b5c3496
Adam Midvidy <amidvidy@gmail.com>
2013-12-17 15:06:45 -0800

    further clarify docstring

--- multimatch/asset.py
+++ multimatch/asset.py
@@ -2,9 +2,11 @@ DELIM = '/'
 
 def make_symbol(*args):
     """
-    Returns a canonical symbol for an arbitrary set of parameters
+    Returns a canonical symbol for an arbitrary set of parameters.
+    Result is independent of argument ordering.
     
     example: make_symbol('BTC', 'USD') => 'BTC/USD'
+             make_symbol('USD', 'BTC') => 'BTC/USD'
     
     """
     
