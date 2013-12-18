c5cb34a1dd224c993aee813f19630bc2a2a536e5
Adam Midvidy <amidvidy@gmail.com>
2013-12-17 18:23:54 -0800

    add __repr__ for Trade

--- multimatch/trade.py
+++ multimatch/trade.py
@@ -5,3 +5,7 @@ class Trade(object):
         self.price = price
         self.buyer_id = buyer_id
         self.seller_id = seller_id
+
+    def __repr__(self):
+        return "Trade(symbol={0}, quantity={1}, price={2}, buyer_id={3}, seller_id={4})".format(
+            self.symbol, self.quantity, self.price, self.buyer_id, self.seller_id)
