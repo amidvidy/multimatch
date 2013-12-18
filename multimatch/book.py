2764fdca4074845e79d4c6b193003d7749658edd
Adam Midvidy <amidvidy@gmail.com>
2013-12-17 17:54:14 -0800

    execute trades at the price of the order that was on the book, also refactoring

--- multimatch/book.py
+++ multimatch/book.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 
 import logging
 
+from itertools import chain
 from collections import deque
 from rbtree import rbtree
 
@@ -38,7 +39,6 @@ class OrderBook(object):
 
     def add_bid(self, bid):
         remaining_bid = self.fill_bid(bid)
-        
         if remaining_bid:
             # Don't let the books get crossed
             assert (not self.has_asks() or self.min_ask() > remaining_bid.price)
@@ -46,62 +46,61 @@ class OrderBook(object):
 
     def add_ask(self, ask):
         remaining_ask = self.fill_ask(ask)
-        
         if remaining_ask:
+            # Don't let the books get crossed
             assert (not self.has_bids() or self.max_bid() < remaining_ask.price)
             self._add_entry(self.asks, remaining_ask.price, remaining_ask)
 
     def fill_bid(self, bid):
-        # get any asks that match our bid
-        askgen = (ask.item for ask in self.asks.iternodes() if ask.key <= bid.price)
-        while bid.quantity:
-            try:
-                (price, asks) = next(askgen)
-                while asks:
-                    # fill asks in the order they were submitted
-                    ask = asks.popleft()
-                    (ask, bid) = self.make_trade(ask, bid)
-                    if ask.quantity:
-                        # we filled our order
-                        # put the partially filled ask back on the list
-                        asks.appendleft(ask)
-                        break
-                # remove empty nodes from the tree
-                if not asks:
-                    del self.asks[price]
+        for (price, asks) in (ask.item for ask in self.asks.iternodes() if bid.price_matches(ask.key)):
+            # remove the current node from the tree
+            # current list of asks is not deleted since it is in scope, yay garbage collection
+            del self.asks[price]
+            for ask in (asks.popleft() for _ in xrange(len(asks))):
+                (ask, bid) = self.make_trade(ask, bid, ask.price)
+                if ask.quantity:
+                    assert bid.quantity == 0
+                    # if we only partially filled the ask, our bid has been filled
+                    # add the partially filled ask to the front of the order list and return
+                    self.asks[price] = deque(chain([ask], asks))
+                    return None
+                if not bid.quantity:
+                    # We filled both orders exactly
+                    return None
 
-            except StopIteration:
-                # order is partially filled, return whatever is unfilled to add to the book
-                return bid
-        
+        assert bid.quantity > 0
+        # We will only reach this point if we failed to fill our order
+        return bid
+
+    # Logic is very similar to fill_bid
+    # TODO: consolidate fill_bid and fill_ask into a generalized match method
     def fill_ask(self, ask):
-        bidgen = (bid.item for bid in self.bids.iternodes() if bid.key >= ask.price)
-        while ask.quantity:
-            try:
-                (price, bids) = next(bidgen)
-                while bids:
-                    bid = bids.popleft()
-                    (ask, bid) = self.make_trade(ask, bid)
-                    if bid.quantity:
-                        # if we only partially filled their order, ours is filled
-                        # put the partially filled bid back on the list
-                        bids.appendleft(bid)
-                        break
-                # remove empty nodes from the tree
-                if not bids:
-                    del self.bids[price]
-
-            except StopIteration:
-                # order is partiailly filled, return whatever is unfilled to add to the bock
-                return ask
-
-    def make_trade(self, ask, bid):
-        assert ask.price <= bid.price
+        for (price, bids) in (bid.item for bid in self.bids.iternodes() if ask.price_matches(bid.key)):
+            # remove the current node from the tree
+            del self.bids[price]
+            for bid in (bids.popleft() for _ in xrange(len(bids))):
+                (ask, bid) = self.make_trade(ask, bid, bid.price)
+                if bid.quantity:
+                    assert ask.quantity == 0
+                    self.bids[price] = deque(chain([bid], bids))
+                    return None
+                if not ask.quantity:
+                    return None
+        
+        assert ask.quantity > 0
+        return ask
+                
+    def make_trade(self, ask, bid, price):
+        # Don't give someone a lower price than what they asked
+        assert price >= ask.price
+        # Don't give someone a higher price than what they bid
+        assert price <= bid.price
+        # Sanity test: make sure the asset is corret
         assert ask.symbol == bid.symbol
         
         quantity = min(ask.quantity, bid.quantity)
         
-        self.trade_callback(Trade(ask.symbol, quantity, ask.price, bid.trader_id, ask.trader_id))
+        self.trade_callback(Trade(ask.symbol, quantity, price, bid.trader_id, ask.trader_id))
 
         # return partials
         return (LimitSellOrder(ask.price, ask.symbol, ask.quantity - quantity, ask.trader_id), 
--- multimatch/test/book_test.py
+++ multimatch/test/book_test.py
@@ -92,15 +92,14 @@ class OrderBookTest(unittest.TestCase):
         self.assertEquals(buy2.trader_id, trade2.buyer_id)
         self.assertEquals(buy3.quantity, trade3.quantity)
         self.assertEquals(buy3.trader_id, trade3.buyer_id)
-        
 
         # make sure all the orders were cleared out
         self.assertFalse(self.book.has_bids(), 'The book should be empty')
         self.assertFalse(self.book.has_asks(), 'The book should be empty')
 
-    def testMatchPartialUnfilled(self):
+    def testMatchBuyPartialUnfilled(self):
         # One seller, one buyer. Buyer should be filled, seller should be partially filled
-        # Order should be filled at the sellers price since it is lower
+        # price should be the sellers since it was on the book first
         
         sell = LimitSellOrder(100, make_symbol('BTC', 'USD'), 1, 'gavin')
         buy = LimitBuyOrder(150, make_symbol('BTC', 'USD'), 0.25, 'satoshi')
@@ -116,9 +115,79 @@ class OrderBookTest(unittest.TestCase):
         self.assertEquals(sell.trader_id, trade.seller_id)
         # Order should be filled at the sellers price since it was on the book first
         self.assertEquals(sell.price, trade.price)
-        print(self.book.asks)
+
         self.assertTrue(self.book.has_asks(), 'There should still be an ask on the book')
 
         self.assertFalse(self.book.has_bids(), 'There should be no bids')
 
+    def testMatchLimitSellPriceOrder(self):
+        # when matching a limit order, the trade executes at the price of the order
+        # that was on the book, not the new order
 
+        # See: http://money.stackexchange.com/questions/15156/how-do-exchanges-match-limit-orders
+
+        # gavin posts an order to buy 5 BTC @ 100 USD each
+        self.book.execute(LimitBuyOrder(100, make_symbol('BTC', 'USD'), 5, 'gavin'))
+        # satoshi posts an order to buy 10 BTC @ 99 USD each
+        self.book.execute(LimitBuyOrder(99, make_symbol('BTC', 'USD'), 10, 'satoshi'))
+        # mhearn posts an order to buy 20 BTC @ 50 USD each
+        self.book.execute(LimitBuyOrder(50, make_symbol('BTC', 'USD'), 20, 'mhearn'))
+
+        # finally, we execute a limit order to sell 100BTC at 0.01 USD
+        self.book.execute(LimitSellOrder(0.01, make_symbol('BTC', 'USD'), 100, 'fool'))
+
+        # all 3 buys should have been filled
+        self.assertEquals(3, self.numOrdersExecuted())
+        [gavin_trade, satoshi_trade, hearn_trade] = self.trades
+
+        # gavin's order should have been filled first at a price of 100
+        self.assertEquals('gavin', gavin_trade.buyer_id)
+        self.assertEquals(100, gavin_trade.price)
+
+        # satoshi's order should have been filled second at a price of 99
+        self.assertEquals('satoshi', satoshi_trade.buyer_id)
+        self.assertEquals(99, satoshi_trade.price)
+
+        # mhearns order should have been filled last at a price of 50
+        self.assertEquals('mhearn', hearn_trade.buyer_id)
+        self.assertEquals(50, hearn_trade.price)
+
+        # All the bids should have been filled
+        self.assertFalse(self.book.has_bids())
+        # Partial order should still be on the book
+        self.assertTrue(self.book.has_asks())
+
+    def testMatchLimitBuyPriceOrder(self):
+        # same as above, but for the opposite situation
+
+        # gavin posts an order to sell 5 BTC @ 100 USD each
+        self.book.execute(LimitSellOrder(100, make_symbol('BTC', 'USD'), 5, 'gavin'))
+        # satoshi posts an order to sell 10 BTC @ 99 USD each
+        self.book.execute(LimitSellOrder(99, make_symbol('BTC', 'USD'), 10, 'satoshi'))
+        # mhearn posts an order to sell 20 BTC @ 50 USD each
+        self.book.execute(LimitSellOrder(50, make_symbol('BTC', 'USD'), 20, 'mhearn'))
+
+        # finally, we execute a limit order to buy 100BTC at 1000 USD
+        self.book.execute(LimitBuyOrder(1000, make_symbol('BTC', 'USD'), 100, 'fool'))
+
+        # all 3 buys should have been filled
+        self.assertEquals(3, self.numOrdersExecuted())
+        [hearn_trade, satoshi_trade, gavin_trade] = self.trades
+
+        # mhearns order should have been filled first at a price of 50
+        self.assertEquals('mhearn', hearn_trade.seller_id)
+        self.assertEquals(50, hearn_trade.price)
+
+        # satoshi's order should have been filled second at a price of 99
+        self.assertEquals('satoshi', satoshi_trade.seller_id)
+        self.assertEquals(99, satoshi_trade.price)
+        
+        # gavin's order should have been filled last at a price of 100
+        self.assertEquals('gavin', gavin_trade.seller_id)
+        self.assertEquals(100, gavin_trade.price)
+
+        # All the asks should have been filled
+        self.assertFalse(self.book.has_asks())
+        # Partial buy order should still be on the book
+        self.assertTrue(self.book.has_bids())
+    
